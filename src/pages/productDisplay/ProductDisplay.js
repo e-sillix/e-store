a1251a5397b741fa457a39b222bf69f4c3a344df
e-sillix <sumitpanwar1807@gmail.com>
2024-02-06 00:56:15 +0530

    working add to cart

--- src/pages/productDisplay/ProductDisplay.js
+++ src/pages/productDisplay/ProductDisplay.js
@@ -2,15 +2,22 @@ import React, { useEffect, useState } from "react";
 import styles from "./ProductDisplay.module.scss";
 import { useParams } from "react-router";
 import { useSelector } from "react-redux";
-import { storage } from "../../firebase/config";
+import { auth, db, storage } from "../../firebase/config";
 import { ref, getDownloadURL } from "firebase/storage";
+import {
+  Firestore,
+  addDoc,
+  collection,
+  doc,
+  getDoc,
+  setDoc,
+  updateDoc,
+} from "firebase/firestore";
 
 export default function ProductDisplay() {
   const { all_products } = useSelector((s) => s.product);
-
   const { productId } = useParams();
   const product = all_products.find((e) => e.ProductID === Number(productId));
-
   const [imageUrl, setImageUrl] = useState(null);
 
   const fetcher = async (suffix) => {
@@ -37,6 +44,44 @@ export default function ProductDisplay() {
     setImageUrl([imageUrl, imagea, imageb, imagec]);
 
     // Now you can use the other image URLs as needed
+  };
+
+  const cartItem = {
+    productId: productId,
+    amount: 1,
+  };
+
+  const addToCart = () => {
+    auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const userId = user.uid;
+        const userDocRef = doc(collection(db, "user"), userId);
+        const userDocSnapshot = await getDoc(userDocRef);
+
+        if (userDocSnapshot.exists()) {
+          const cartCollection = collection(userDocRef, "cart");
+          const cartItemDocRef = doc(cartCollection, productId);
+          const cartItemDocSnapshot = await getDoc(cartItemDocRef);
+
+          if (cartItemDocSnapshot.exists()) {
+            const currentAmount = cartItemDocSnapshot.data().amount || 0;
+            const updatedAmount = currentAmount + 1;
+            await updateDoc(cartItemDocRef, { amount: updatedAmount });
+          } else {
+            await setDoc(cartItemDocRef, { ...cartItem, amount: 1 });
+          }
+        } else {
+          const usersCollection = collection(db, "user");
+          const newUserDocRef = doc(usersCollection, userId);
+          const cartCollection = collection(newUserDocRef, "cart");
+
+          await setDoc(newUserDocRef, { userId });
+          await setDoc(doc(cartCollection, productId), cartItem);
+        }
+      } else {
+        console.log("No user is signed in.");
+      }
+    });
   };
 
   useEffect(() => {
@@ -66,13 +111,13 @@ export default function ProductDisplay() {
           </div>
         </div>
         <div>
-          <button>Add To Cart</button>
+          <button onClick={addToCart}>Add To Cart</button>
         </div>
       </div>
       <div className={styles.productImage}>
         {imageUrl && (
           <div>
-            {imageUrl[0] && <img src={imageUrl[0]} alt="product" />}
+            {imageUrl[0] && <img width={150} src={imageUrl[0]} alt="product" />}
             {imageUrl[1] && <img src={imageUrl[1]} alt="product" />}
             {imageUrl[2] && <img src={imageUrl[2]} alt="product" />}
             {imageUrl[3] && <img src={imageUrl[3]} alt="product" />}
